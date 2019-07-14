f567176e1d1882478b266625a329442f499aaf5c
moniz1 <moniz.nguyen@gmail.com>
2019-07-14 16:53:13 +0700

    Initial commit

---
+++ src/example.js
@@ -0,0 +1,87 @@
+import React, { useEffect } from "react";
+import Select, { components } from "react-select";
+import { colourOptions } from "./docs/data";
+
+const controlStyles = {
+  padding: "5px",
+  flexGrow: "2",
+  border: "none"
+};
+
+const containerStyles = {
+  display: "flex",
+  border: "1px solid #ccc"
+};
+const MultiValueContainer = props => null;
+
+const ClearIndicator = props => null;
+
+export default () => {
+  const ControlComponent = props => {
+    const inputRef = React.createRef();
+    useEffect(() => {
+      if (isShown) inputRef.current.focus();
+    }, [inputRef]);
+
+    const selectedValue = props
+      .getValue()
+      .map(t => props.selectProps.getOptionLabel(t))
+      .join(", ");
+    return (
+      <React.Fragment>
+        <section style={containerStyles}>
+          <input
+            ref={inputRef}
+            onClick={() => setIsShown(!isShown)}
+            onBlur={() => setIsShown(false)}
+            style={controlStyles}
+            value={selectedValue}
+          />
+          <div class=" css-1wy0on6">
+            <span class=" css-1okebmr-indicatorSeparator" />
+            <div aria-hidden="true" class=" css-tlfecz-indicatorContainer">
+              <svg
+                height="20"
+                width="20"
+                viewBox="0 0 20 20"
+                aria-hidden="true"
+                focusable="false"
+                class="css-19bqh2r"
+              >
+                <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z" />
+              </svg>
+            </div>
+          </div>
+        </section>
+        <components.Control {...props} />
+      </React.Fragment>
+    );
+  };
+  const [isShown, setIsShown] = React.useState(false);
+
+  return (
+    <Select
+      styles={{
+        control: (str, props) => {
+          return Object.assign(str, {
+            marginTop: "5px",
+            display: isShown ? "" : "none"
+          });
+        }
+      }}
+      closeMenuOnSelect={false}
+      hideSelectedOptions={false}
+      components={{
+        MultiValueContainer,
+        Control: ControlComponent,
+        ClearIndicator,
+        DropdownIndicator: ClearIndicator,
+        IndicatorSeparator: ClearIndicator
+      }}
+      defaultValue={[colourOptions[4], colourOptions[5]]}
+      isMulti
+      options={colourOptions}
+      menuIsOpen={isShown}
+    />
+  );
+};
